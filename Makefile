fd78bb4ff260561a4f38367c66485b9ff930da78
karrmagadgeteer2 <martin.karrin@captor.se>
2023-03-25 10:20:16 +0100

    dev scripts updated

--- Makefile
+++ Makefile
@@ -3,5 +3,14 @@ venv:
 	venv/bin/python --version
 	venv/bin/pip install --upgrade pip
 	venv/bin/pip install poetry==1.4.0
+	export PYTHONPATH=$PYTHONPATH:${PWD}
+	source venv/bin/activate
 	poetry install --with dev
-	poetry check
+
+active:
+	export PYTHONPATH=$PYTHONPATH:${PWD}
+	source venv/bin/activate
+
+clean:
+	deactivate
+	rm -rf venv
---
+++ make.ps1
@@ -0,0 +1,24 @@
+param (
+    [string]$task = "active"
+)
+
+if ($task -eq "active") {
+    # run commands to activate virtual environment
+    $env:PYTHONPATH = "$env:PYTHONPATH;$pwd"
+    .\venv\Scripts\activate
+} elseif ($task -eq "make") {
+    # make virtual environment
+    python -m venv ./venv
+    $env:PYTHONPATH = "$env:PYTHONPATH;$pwd"
+    .\venv\Scripts\activate
+    python -m pip install --upgrade pip
+    pip install poetry==1.4.0
+    poetry install --with dev
+} elseif ($task -eq "clean") {
+    # remove virtual environment to start over
+    deactivate
+    Remove-Item -Path ".\venv" -Recurse -Force
+} else {
+    # invalid task argument
+    Write-Host "Only active, make or clean are allowed as tasks"
+}
