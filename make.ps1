c7e2d9079e8bfdaded8d1d8ad486cf127ee447de
Martin Karrin <martin.karrin@captor.se>
2024-05-04 15:53:51 +0200

    improved shell scripts

--- make.ps1
+++ make.ps1
@@ -3,14 +3,49 @@ param (
 )
 
 if ($task -eq "active") {
-    # run commands to activate virtual environment
-    $env:PYTHONPATH = "$env:PYTHONPATH;$pwd"
+    if ($null -ne $env:PYTHONPATH)
+    {
+        if (-not $env:PYTHONPATH -match [regex]::Escape($PWD))
+        {
+            $env:PYTHONPATH = "$PWD" + ";$env:PYTHONPATH"
+            Write-Output "`nPYTHONPATH changed. It is '$( $env:PYTHONPATH )'"
+        }
+        else
+        {
+            Write-Output "`nPYTHONPATH not changed. It is '$( $env:PYTHONPATH )'"
+        }
+    }
+    else
+    {
+        $env:PYTHONPATH = $PWD
+        Write-Output "`nPYTHONPATH set to: $( $env:PYTHONPATH )"
+    }
     .\venv\Scripts\activate
+    Write-Output "`nThe Python used in the '$(Split-Path -Leaf $env:VIRTUAL_ENV)' environment is:"
+    Get-Command python
 } elseif ($task -eq "make") {
-    # make virtual environment
+    Remove-Item -Path ".\venv" -Recurse -Force -ErrorAction SilentlyContinue
     python -m venv ./venv
-    $env:PYTHONPATH = "$env:PYTHONPATH;$pwd"
+    if ($null -ne $env:PYTHONPATH)
+    {
+        if (-not $env:PYTHONPATH -match [regex]::Escape($PWD))
+        {
+            $env:PYTHONPATH = "$PWD" + ";$env:PYTHONPATH"
+            Write-Output "`nPYTHONPATH changed. It is '$( $env:PYTHONPATH )'"
+        }
+        else
+        {
+            Write-Output "`nPYTHONPATH not changed. It is '$( $env:PYTHONPATH )'"
+        }
+    }
+    else
+    {
+        $env:PYTHONPATH = $PWD
+        Write-Output "`nPYTHONPATH set to: $( $env:PYTHONPATH )"
+    }
     .\venv\Scripts\activate
+    Write-Output "`nThe Python used in the '$(Split-Path -Leaf $env:VIRTUAL_ENV)' environment is:"
+    Get-Command python
     python.exe -m pip install --upgrade pip
     pip install poetry==1.8.2
     Remove-Item -Path 'poetry.lock' -Force -ErrorAction SilentlyContinue
